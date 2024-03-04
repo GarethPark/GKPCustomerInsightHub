8fdaef7b6aeacf978703220d6ae3cd7f59e8e7ec
Gareth Park <garethpark@yahoo.com>
2024-03-04 22:52:55 +0000

    add customer profileviewer js

--- force-app/main/default/lwc/customerProfileViewer/customerProfileViewer.js
+++ force-app/main/default/lwc/customerProfileViewer/customerProfileViewer.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { api, wire } from 'lwc';
 
-export default class CustomerProfileViewer extends LightningElement {}
+const CONTACT_FIELDS = ['Contact.Name', 'Contact.Email', 'Contact.Phone'];
+const ACCOUNT_FIELDS = ['Account.Name', 'Account.Industry'];
+
+export default class CustomerProfileViewer extends LightningElement {
+    @api recordId;
+
+    @wire(getRecord, { recordId: '$recordId', fields: CONTACT_FIELDS })
+    contact;
+
+    @wire(getRecord, { recordId: '$recordId', fields: ACCOUNT_FIELDS })
+    account;
+}
