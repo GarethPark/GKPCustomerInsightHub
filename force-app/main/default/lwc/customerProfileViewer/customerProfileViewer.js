dd6d1e7725bb73587e817d75a9524f55b997cf40
Gareth Park <garethpark@yahoo.com>
2024-03-11 21:40:57 +0000

    added additional contact fields and getters

--- force-app/main/default/lwc/customerProfileViewer/customerProfileViewer.js
+++ force-app/main/default/lwc/customerProfileViewer/customerProfileViewer.js
@@ -3,7 +3,19 @@ import { getRecord } from 'lightning/uiRecordApi';
 import { api, wire } from 'lwc';
 import { getFieldValue } from 'lightning/uiRecordApi';
 
-const CONTACT_FIELDS = ['Contact.Name', 'Contact.Email', 'Contact.Phone'];
+const CONTACT_FIELDS = [
+    'Name',
+    'Email',
+    'Phone',
+    'Title', // Job Title
+    'MailingStreet',
+    'MailingCity',
+    'MailingState',
+    'MailingPostalCode',
+    'MailingCountry',
+    'Birthdate',
+    'Description'
+];
 
 export default class CustomerProfileViewer extends LightningElement {
     @api recordId;
@@ -14,6 +26,27 @@ export default class CustomerProfileViewer extends LightningElement {
     get name() {
         //console.log('name record id ' + this.recordId);
         return this.contact.data.fields.Name.value;
+    }
+
+    get jobTitle() {
+        return getFieldValue(this.contact.data, 'Title');
+    }
+
+    get mailingAddress() {
+        const street = getFieldValue(this.contact.data, 'MailingStreet');
+        const city = getFieldValue(this.contact.data, 'MailingCity');
+        const state = getFieldValue(this.contact.data, 'MailingState');
+        const postalCode = getFieldValue(this.contact.data, 'MailingPostalCode');
+        const country = getFieldValue(this.contact.data, 'MailingCountry');
+        return `${street}, ${city}, ${state}, ${postalCode}, ${country}`;
+    }
+
+    get birthdate() {
+        return getFieldValue(this.contact.data, 'Birthdate');
+    }
+
+    get description() {
+        return getFieldValue(this.contact.data, 'Description');
     }
 
     connectedCallback() {
